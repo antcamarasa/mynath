b3b4d9e66fe58808deb41a2c778eeea73cc74694
souraya cassam chenai <sourayacc@gmail.com>
2022-03-17 16:45:04 +0400

    working progress feedback

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,63 @@
+class FeedbacksController < ApplicationController
+  before_action :set_feedback, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: :index
+
+
+  # GET /feedbacks for appointment_id
+  def index
+    #@feedbacks = Feedback.where(appointment_id: @appointment)
+    @appointment = Appointment.first
+    @feedbacks = @appointment.feedbacks
+    @feedback = Feedback.new
+  end
+
+  # GET /feedbackes/1
+  def show
+  end
+
+  # GET /feedbackes/new
+  def new
+    @feedback = Feedback.new
+  end
+
+  # GET /feedbackes/1/edit
+  def edit
+  end
+
+  # POST /feedbackes
+  def create
+    @appointment = Appointment.first
+    @feedback = Feedback.new(feedback_params)
+    @feedback.appointment = @appointment
+    if @feedback.save
+      redirect_to appointment_feedbacks_path, notice: 'feedback was successfully created.'
+    else
+      render :index
+    end
+  end
+
+  # PATCH/PUT /restaurants/1
+  def update
+    if @feedback.update(feedback_params)
+      redirect_to @feedback, notice: 'feedback was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /restaurants/1
+  def destroy
+    @feedback.destroy
+    redirect_to feedbackes_url, notice: 'feedback was successfully destroyed.'
+  end
+
+  private
+
+  def set_feedback
+    @feedback = Feedback.find(params[:id])
+  end
+
+  def feedback_params
+    params.require(:feedback).permit(:review_patient, :review_therapist)
+  end
+end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,7 +2,7 @@ class Appointment < ApplicationRecord
   belongs_to :user, foreign_key: :users_id
   belongs_to :therapist, class_name: "User", foreign_key: :therapists_id
   has_many :prescriptions#, dependent: :destroy
-  has_many :feedbacks#, dependent: :destroy
+  has_many :feedbacks, dependent: :destroy
 
   validates :date_time, presence: true
 end
