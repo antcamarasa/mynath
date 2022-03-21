ac6a8789849952e1c1af6536ca02f635a4f5e72b
Josephine Amigues <josephine.amigues@gmail.com>
2022-03-21 11:52:09 +0400

    Index therapist

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,9 @@ class PagesController < ApplicationController
   def index
     @therapists = User.where(therapist: true)
     @results = @therapists.search_by_name_and_speciality(params[:query])
+    if @results == nil
+      redirect_to therapists_path(@therapist)
+    end
   end
 
   def visio
