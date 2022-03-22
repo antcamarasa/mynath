eabaa0fc6753f9da36c1a96b56288ac4d80adb60
Antoine <camarasa.ltd@gmail.com>
2022-03-22 12:08:43 +0400

    bug twilio

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,5 @@
 class Appointment < ApplicationRecord
+  require 'twilio-ruby'
   belongs_to :user, foreign_key: :users_id
   belongs_to :therapist, class_name: "User", foreign_key: :therapists_id
   has_many :prescriptions, dependent: :destroy
@@ -12,7 +13,7 @@ class Appointment < ApplicationRecord
   # callback ActiveRecord
   def set_room_name
     client = Twilio::REST::Client.new(ENV["KEY_ID"], ENV["AUTH_TOKEN"])
-    room = client.video.rooms.create(unique_name: "#{self.id}_#{Time.now.to_i}")
+    room = client.video.rooms.create(unique_name: "#{self.users_id}_#{Time.now.to_i}")
     self.url_room = room.unique_name
   end
 end
